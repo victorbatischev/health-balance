10b9bbe58a5929393106e0e4f8f3903bb170aa84
Yakubov <ykuvonchbek@outlook.com>
2022-11-02 01:44:50 +0500

    added device admin

--- android/app/src/main/java/com/academia/health/PedometerPlugin.java
+++ android/app/src/main/java/com/academia/health/PedometerPlugin.java
@@ -1,16 +1,31 @@
 package com.academia.health;
 
 import android.Manifest;
+import android.app.admin.DevicePolicyManager;
+import android.app.admin.SystemUpdatePolicy;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.Settings;
+import android.util.Log;
 
 import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.ActivityResultRegistry;
+import androidx.activity.result.contract.ActivityResultContract;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityOptionsCompat;
 import androidx.core.content.ContextCompat;
 
+import com.academia.health.utils.AdminManager;
 import com.academia.health.utils.SharedPrefManager;
 import com.getcapacitor.JSObject;
 import com.getcapacitor.NativePlugin;
@@ -29,13 +44,38 @@ public class PedometerPlugin extends Plugin {
     private PedometerPluginImpl plugin;
     private ActivityResultLauncher<String> requestPermissionLauncher;
 
+    private Boolean isDialogPresent = false;
+    private Boolean isStartInvoked = false;
+
+    private DevicePolicyManager deviceManger;
+    private ComponentName compName;
+
     @Override
     public void load() {
         super.load();
 
+        compName = new ComponentName(getActivity(), AdminManager.class);
+        deviceManger = (DevicePolicyManager) getContext().getSystemService(
+                Context.DEVICE_POLICY_SERVICE);
+
+        if(deviceManger.isDeviceOwnerApp(getContext().getPackageName())){
+//            setDefaultCosuPolicies(true);
+        } else {
+            Log.e("TAG","This application not whitelisted");
+        }
+
         requestPermissionLauncher =
                 getActivity().registerForActivityResult(new ActivityResultContracts
-                        .RequestPermission(), isGranted -> {});
+                        .RequestPermission(), isGranted -> {
+                    if (isGranted) {
+
+                    } else {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            askPermission();
+                        }
+                    }
+
+                });
 
         plugin = PedometerPluginImpl.getInstance();
         plugin.initialize(getContext());
@@ -50,11 +90,17 @@ public class PedometerPlugin extends Plugin {
             };
         };
 
-        SharedPrefManager manager = new SharedPrefManager(getContext());
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !manager.isBatteryOptDisAsked()) {
-            getContext().startActivity(new Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS,
-                    Uri.parse("package:" +getContext().getPackageName())));
-            manager.setBatteryOptimizationDisabled(true);
+        isDialogPresent = false;
+        isStartInvoked = false;
+    }
+
+    @Override
+    protected void handleOnStart() {
+        super.handleOnStart();
+        if (isStartInvoked) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                askPermission();
+            }
         }
     }
 
@@ -91,16 +137,12 @@ public class PedometerPlugin extends Plugin {
 
     @PluginMethod
     public void start(PluginCall call) {
-        SharedPrefManager sharedPrefManager = new SharedPrefManager(getContext());
-        String lastSavedSteps = String.valueOf(sharedPrefManager.getLastNumberOfSteps());
-        ForegroundService.startService(getContext(), lastSavedSteps);
-
-        if(ContextCompat.checkSelfPermission(getActivity(),
-                android.Manifest.permission.ACTIVITY_RECOGNITION) ==
-                PackageManager.PERMISSION_GRANTED){
-            plugin.start();
+        askDeviceAdmin();
+        isStartInvoked = true;
+        askBatteryOptPermission();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            askPermission();
         }
-        call.resolve();
     }
 
     @PluginMethod
@@ -120,5 +162,64 @@ public class PedometerPlugin extends Plugin {
     public void requestPermission(PluginCall call) {
         requestPermissionLauncher.launch(android.Manifest.permission.ACTIVITY_RECOGNITION);
         call.resolve();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void askPermission() {
+        if(ContextCompat.checkSelfPermission(getActivity(),
+                android.Manifest.permission.ACTIVITY_RECOGNITION) ==
+                PackageManager.PERMISSION_GRANTED) {
+            startExecution();
+        } else if (getActivity().shouldShowRequestPermissionRationale(android.Manifest.permission.ACTIVITY_RECOGNITION)) {
+            requestPermissionLauncher.launch(android.Manifest.permission.ACTIVITY_RECOGNITION);
+        } else {
+            showSettingsDialog();
+        }
+    }
+
+    private void showSettingsDialog() {
+        if (isDialogPresent) return;
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setCancelable(false);
+        builder.setTitle("Необходимо разрешение");
+        builder.setMessage("Вы должны разрешить физическую активность для использования этого приложения");
+        builder.setPositiveButton("Настройки", (dialog, which) -> {
+            openSettings();
+            isDialogPresent = false;
+        });
+        builder.create().show();
+        isDialogPresent = true;
+    }
+
+    private void openSettings() {
+        Intent intent = new Intent();
+        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        intent.setData(Uri.fromParts("package", BuildConfig.APPLICATION_ID, null));
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        getContext().startActivity(intent);
+    }
+
+    private void startExecution() {
+        SharedPrefManager sharedPrefManager = new SharedPrefManager(getContext());
+        String lastSavedSteps = String.valueOf(sharedPrefManager.getLastNumberOfSteps());
+        ForegroundService.startService(getContext(), lastSavedSteps);
+
+        plugin.start();
+    }
+
+    private void askBatteryOptPermission() {
+        SharedPrefManager manager = new SharedPrefManager(getContext());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !manager.isBatteryOptDisAsked()) {
+            getContext().startActivity(new Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS,
+                    Uri.parse("package:" +getContext().getPackageName())));
+            manager.setBatteryOptimizationDisabled(true);
+        }
+    }
+
+    private void askDeviceAdmin() {
+        Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+        intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, compName);
+        intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION, "Вы должны предоставить этот доступ, чтобы сделать рабочее приложение стабильным");
+        startActivityForResult(null, intent, 2);
     }
 }
---
+++ android/app/src/main/java/com/academia/health/utils/AdminManager.java
@@ -0,0 +1,7 @@
+package com.academia.health.utils;
+
+import android.app.admin.DeviceAdminReceiver;
+
+public class AdminManager extends DeviceAdminReceiver {
+
+}
